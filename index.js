bdbc271ab18b2e8a20c27610e44ef298bb8955e2
Derrick-MUGISHA <derrickmugisha169@gmail.com>
2026-02-17 15:02:57 +0200

    first Example

---
+++ index.js
@@ -0,0 +1,20 @@
+
+//  Exercise 1 : array initialization and property Access
+
+const productCategories = ["Books", "Electronics", "Clothing", "Home & Kitchen", "Toys & Games"];
+
+//  a 
+console.log(productCategories[0]);
+// b
+console.log(productCategories[productCategories.length - 1]);
+// c
+console.log(productCategories.length);
+
+// d
+
+productCategories[1] = "Pen";
+//  e
+console.log(productCategories);
+
+
+//  Exercise 2 : 
